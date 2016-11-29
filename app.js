3b2cbac92af75f585dd18d677e72de3e84174f14
Taj English <tajenglish@gmail.com>
2016-11-28 20:52:25 -0500

    Login and sign up complete and fully functional

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var express = require('express'),
   pgp = require('pg-promise')(),
   db = pgp("postgres:tajenglish@localhost:5432/quedj"),
   methodOverride = require('method-override'),
-  bdPars = require('body-parser');
-bcrypt = require('bcrypt');
+  bdPars = require('body-parser'),
+  bcrypt = require('bcrypt'),
+  session = require('express-session');
 
 //configure express and related packages
 app.engine('html', mustache());
@@ -19,6 +20,15 @@ app.use(bdPars.urlencoded({
 })); //body parser
 app.use(bdPars.json()); //body parser
 
+app.use(session({
+  secret: 'test',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false
+  }
+}));
+
 //start the server
 var port = 3000;
 app.listen(port, function() {
@@ -30,15 +40,53 @@ app.get('/', function(req, res) {
   res.render('home/index');
 });
 
+app.get('/login', function(req, res) {
+  res.render('home/login');
+});
+
+
+app.post('/login', function(req, res) {
+  var data = req.body;
+  console.log(data);
+
+  db.one(
+    "(SELECT * FROM fans where email = $1) UNION (SELECT * FROM djs where email = $1)", [data.email]).catch(function() {
+    res.send('Email/Password not found.');
+  }).then(function(user) {
+    console.log(user);
+     console.log(user.password);
+      console.log(data.password);
+    bcrypt.compare(data.password, user.password, function(err, cmp) {
+      if (cmp) {
+        req.session.user = user;
+        res.redirect('/index');
+      } else {
+        res.send('Email/Password not found.');
+      }
+    });
+  });
+});
+
+
 app.post('/register', function(req, res) {
   var data = req.body;
   console.log(data);
+  if(data.accountPicker === "dj"){
   bcrypt.hash(data.password, 10, function(err, hash) {
-    db.none("INSERT INTO users (first_name,last_name,type,email,location,password) VAlUES ($1,$2,$3,$4,$5,$6)",
+    db.none("INSERT INTO djs (First_Name,Last_Name,type,email,location,password) VAlUES ($1,$2,$3,$4,$5,$6)",
     [data.first_name, data.last_name, data.accountPicker, data.email, data.location, hash]).then(function(data) {
       console.log(data);
       res.redirect("/");
       });
   });
+}else{
+  bcrypt.hash(data.password, 10, function(err, hash) {
+    db.none("INSERT INTO fans (First_Name,Last_Name,type,email,location,password) VAlUES ($1,$2,$3,$4,$5,$6)",
+    [data.first_name, data.last_name, data.accountPicker, data.email, data.location, hash]).then(function(data) {
+      console.log(data);
+      res.redirect("/");
+      });
+  });
+}
 
 });
--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,22 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS que;
 
-CREATE TABLE users(
+CREATE TABLE djs(
+  id SERIAL PRIMARY KEY,
+  first_name varchar(250) NOT NULL,
+  last_name varchar(250) NOT NULL,
+  email varchar(250) NOT NULL,
+  image varchar(250),
+  bio varchar(250),
+  location varchar(250) NOT NULL,
+  password varchar(250) NOT NULL,
+  events varchar(250),
+  type varchar(3) NOT NULL,
+  upvotes integer,
+  downvotes integer
+);
+
+CREATE TABLE fans(
   id SERIAL PRIMARY KEY,
   first_name varchar(250) NOT NULL,
   last_name varchar(250) NOT NULL,
@@ -21,5 +36,6 @@ CREATE TABLE que (
   title varchar(250) NOT NULL,
   artist varchar(250) NOT NULL,
   img varchar(250) NOT NULL,
-  que_id integer REFERENCES users(id)
+  djs_id integer REFERENCES djs(id),
+  fans_id integer REFERENCES fans(id)
 );
