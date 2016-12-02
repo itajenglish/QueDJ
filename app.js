d28738933db952cfbf6f62f0415db1c7acd84018
Taj English <tajenglish@gmail.com>
2016-12-02 14:13:39 -0500

    MVP working still alot of bugs to fix and code refactoring

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var express = require('express'),
   bdPars = require('body-parser'),
   bcrypt = require('bcrypt'),
   session = require('express-session');
-  //fetch = require("fetch")
-  fetch = require('node-fetch');
+//fetch = require("fetch")
+fetch = require('node-fetch');
 
 
 //configure express and related packages
@@ -17,7 +17,7 @@ app.engine('html', mustache());
 app.set('view engine', 'html');
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
-app.use(methodOverride('_method')); //method override
+app.use(methodOverride('__method')); //method override
 app.use(bdPars.urlencoded({
   extended: false
 })); //body parser
@@ -50,13 +50,17 @@ app.get('/', function(req, res) {
     } else if (user.type === "dj") {
       res.redirect("/djboard");
     }
+  } else {
+    db.any('SELECT first_name,last_name,bio,image,location FROM djs')
+    .then(function(data){
+      res.render('home/index', {data: data});
+    })
   }
-  res.render('home/index');
 });
 
 //Register Route
 app.post('/register', function(req, res) {
-  function capitalFunc(string){
+  function capitalFunc(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
   var data = req.body;
@@ -65,21 +69,39 @@ app.post('/register', function(req, res) {
   var image = "images/Music-Dj-icon.png"
   if (data.accountPicker === "dj") {
     bcrypt.hash(data.password, 10, function(err, hash) {
-      db.none("INSERT INTO djs (First_Name,Last_Name,type,email,image,location,password) VAlUES ($1,$2,$3,$4,$5,$6,$7)",
-      [fname, lname, data.accountPicker, data.email, image, data.location, hash]).then(function(data) {
+      db.none("INSERT INTO djs (First_Name,Last_Name,type,email,image,location,password) VAlUES ($1,$2,$3,$4,$5,$6,$7)", [fname, lname, data.accountPicker, data.email, image, data.location, hash]).then(function(data) {
         console.log(data);
         res.redirect("/login");
       });
     });
   } else {
     bcrypt.hash(data.password, 10, function(err, hash) {
-      db.none("INSERT INTO fans (First_Name,Last_Name,type,email,image,location,password) VAlUES ($1,$2,$3,$4,$5,$6,$7)",
-      [fname, lname, data.accountPicker, data.email, image, data.location, hash]).then(function(data) {
+      db.none("INSERT INTO fans (First_Name,Last_Name,type,email,image,location,password) VAlUES ($1,$2,$3,$4,$5,$6,$7)", [fname, lname, data.accountPicker, data.email, image, data.location, hash]).then(function(data) {
         console.log(data);
         res.redirect("/login");
       });
     });
   }
+});
+
+//Update Info Route
+app.put('/updateInfo',function(req,res){
+  function capitalFunc(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  var data = req.body;
+  var userID = req.session.user.id;
+  var fname = capitalFunc(data.first_name);
+  var lname = capitalFunc(data.last_name);
+  var loc = data.location;
+  var bio = data.bio;
+  console.log(data);
+  db.one('UPDATE djs SET (first_name,last_name,location,bio) = ($1,$2,$3,$4) WHERE id = $5 returning first_name,last_name',
+[fname,lname,loc,bio,userID]).then(function(dataReturn){
+  console.log(dataReturn)
+  res.redirect('/dj/'+ dataReturn.first_name + '-' + dataReturn.last_name);
+});
+
 });
 
 //Login Routes
@@ -93,19 +115,19 @@ app.post('/login', function(req, res) {
   console.log(data);
 
   db.one(
-    "(SELECT * FROM fans where email = $1) UNION (SELECT * FROM djs where email = $1)", [data.email])
+      "(SELECT * FROM fans where email = $1) UNION (SELECT * FROM djs where email = $1)", [data.email])
     .then(function(user) {
-    bcrypt.compare(data.password, user.password, function(err, cmp) {
-      if (cmp) {
-        req.session.user = user;
-        res.redirect('/');
-      } else {
-        res.send('Email/Password not found.');
-      }
-    });
-  }).catch(function() {
-    res.send('Email/Password not found.');
-  })
+      bcrypt.compare(data.password, user.password, function(err, cmp) {
+        if (cmp) {
+          req.session.user = user;
+          res.redirect('/');
+        } else {
+          res.send('Email/Password not found.');
+        }
+      });
+    }).catch(function() {
+      res.send('Email/Password not found.');
+    })
 })
 
 //Dashboard Routes
@@ -115,7 +137,7 @@ app.get('/djboard', function(req, res) {
 
   if (user === undefined) {
     res.redirect('/');
-  } else if(user.type === 'dj') {
+  } else if (user.type === 'dj') {
     res.render('dashboards/djboard');
   }
 });
@@ -123,79 +145,111 @@ app.get('/djboard', function(req, res) {
 app.get('/userboard', function(req, res) {
   var email;
   var user = req.session.user;
-  var data = {data:user};
+  var data = {
+    data: user
+  };
 
   if (user === undefined) {
     res.redirect('/');
-  } else if(user.type === 'fan') {
+  } else if (user.type === 'fan') {
     res.render('dashboards/userboard', data);
+  } else {
+    res.send('neither clause met');
   }
 });
 
 //Profile Routes
-app.get('/user/:fname-:lname',function(req,res){
-function capitalFunc(string){
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-var fname = capitalFunc(req.params.fname);
-var lname = capitalFunc(req.params.lname)
-db.one('SELECT * FROM djs WHERE first_name = $1 AND last_name = $2',[fname,lname])
-.then(function(data){
-  console.log(data);
-  var dbData = {dj:data};
-  res.render('profiles/userprofile', dbData);
+app.get('/user/:fname-:lname', function(req, res) {
+  function capitalFunc(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  var fname = capitalFunc(req.params.fname);
+  var lname = capitalFunc(req.params.lname)
+  db.one('SELECT * FROM djs WHERE first_name = $1 AND last_name = $2', [fname, lname])
+    .then(function(data) {
+      console.log(data);
+      var dbData = {
+        dj: data
+      };
+      res.render('profiles/userprofile', dbData);
+    });
 });
-  });
+
+app.get('/dj/:fname-:lname', function(req, res) {
+  var user = req.session.user;
+  if (user === undefined) {
+    res.redirect('/login');
+
+  } else if (user.type === 'dj') {
+    var capitalFunc = function(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    var fname = capitalFunc(req.params.fname);
+    var lname = capitalFunc(req.params.lname);
+
+
+    db.one('SELECT * FROM djs WHERE first_name = $1 AND last_name = $2', [fname, lname])
+      .then(function(data) {
+        console.log(data);
+        var dbData = {
+          dj: data
+        };
+        res.render('profiles/djprofile', dbData);
+      });
+  } else if (user.type === 'fan') {
+    res.redirect('/userboard');
+  }
+});
 
 //Api Routes
 
-app.get('/api',function(req,res){
-db.any('SELECT id,first_name,last_name,image,location FROM djs')
-.then(function(data){
-  var data = JSON.stringify(data);
-  res.send(data);
-});
+app.get('/api', function(req, res) {
+  db.any('SELECT id,first_name,last_name,image,location FROM djs')
+    .then(function(data) {
+      var data = JSON.stringify(data);
+      res.send(data);
+    });
 });
 
 
-app.get('/api/:name',function(req,res){
+app.get('/api/:name', function(req, res) {
   var name = req.params.name
-  name = name.replace('%20',' ')
+  name = name.replace('%20', ' ')
   name = name.split(' ')
-  db.any('SELECT id,first_name,last_name,image,location,bio FROM djs WHERE first_name = $1 OR last_name = $2', [name[0],name[1]])
-  .then(function(data){
-    console.log(data)
-    res.send(data);
-  });
+  db.any('SELECT id,first_name,last_name,image,location,bio FROM djs WHERE first_name = $1 OR last_name = $2', [name[0], name[1]])
+    .then(function(data) {
+      console.log(data)
+      res.send(data);
+    });
 })
 
-app.get('/search',function(req,res){
+app.get('/search', function(req, res) {
   db.any('SELECT id,first_name,last_name,image,location FROM djs')
-  .then(function(data){
-    var obj = {};
-    var data2 = data.forEach(function(value,index,arry){
-      var fname = value.first_name;
-      var lname = value.last_name;
-      var loc = value.location
-      obj[fname + " "  + lname] = null;
-      // obj[location] = loc;
-    })
-    res.send(obj);
-  });
+    .then(function(data) {
+      var obj = {};
+      var data2 = data.forEach(function(value, index, arry) {
+        var fname = value.first_name;
+        var lname = value.last_name;
+        var loc = value.location
+        obj[fname + " " + lname] = null;
+        // obj[location] = loc;
+      })
+      res.send(obj);
+    });
 });
 
-app.post('/itunes',function(req,res){
+app.post('/itunes', function(req, res) {
   var body = req.body;
   fetch("https://itunes.apple.com/search?term=" + body.title)
-    .then(function(res2){
+    .then(function(res2) {
       return res2.json();
-    }).then(function(json){
+    }).then(function(json) {
       res.json(json);
     })
 
 });
 
-app.post('/saveQueData',function(req,res){
+app.post('/saveQueData', function(req, res) {
   data = req.body;
   var id = data.djID;
   var Title = data.Title;
@@ -203,27 +257,25 @@ app.post('/saveQueData',function(req,res){
   var Album = data.Album;
   var Image = data.Image;
   var userID = req.session.user.id;
-  db.none('INSERT INTO QUE (title,artist,album,img,djs_id,fans_id) VALUES ($1,$2,$3,$4,$5,$6)',
-  [Title,Artist,Album,Image,id,userID]).then(function(){
+  db.none('INSERT INTO QUE (title,artist,album,img,djs_id,fans_id) VALUES ($1,$2,$3,$4,$5,$6)', [Title, Artist, Album, Image, id, userID]).then(function() {
     res.send("Song Added!")
   })
 
 });
 
-app.delete('/deleteQueData',function(req,res){
+app.delete('/deleteQueData', function(req, res) {
   var user = req.session.user.id;
   var songID = req.body.songID;
   console.log(req.body.songID);
-  db.none('DELETE FROM que WHERE id = $1 AND djs_id = $2',
-[songID,user]).then(function(){
-  console.log("Song Deleted");
-});
+  db.none('DELETE FROM que WHERE id = $1 AND djs_id = $2', [songID, user]).then(function() {
+    console.log("Song Deleted");
+  });
 });
 
-app.get('/queData',function(req,res){
+app.get('/queData', function(req, res) {
   var userID = req.session.user.id;
   db.any('SELECT * FROM que WHERE djs_id = $1', [userID])
-  .then(function(data){
-    res.send(data);
-  })
+    .then(function(data) {
+      res.send(data);
+    })
 });
