bc1081faa169b08d04c13c6a71c15fb026456410
Taj English <tajenglish@gmail.com>
2016-12-04 15:15:15 -0500

    3rd Deploy

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(session({
 }));
 
 //start the server
-var PORT = proecess.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
   console.log('Server running on port ' + port + '!');
 });
