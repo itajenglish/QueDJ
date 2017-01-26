f9a77562c543eba8c000075860aafbc3f1c49d29
Taj English <tajenglish@gmail.com>
2017-01-25 22:24:36 -0500

    Update

--- public/scripts/djboard.js
+++ public/scripts/djboard.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   var searchBox = $("#autocomplete-input");
-    var searchBtn = $('#searchBtn');
+  var searchBtn = $('#searchBtn');
 
-    var getQue = function(){
-      $.ajax({
+  var getQue = function() {
+    $.ajax({
         url: 'https://quedj.herokuapp.com/queData/',
         type: 'GET',
       })
@@ -15,36 +15,38 @@ $(document).ready(function() {
   getQue();
 
 
-      var parseData = function(data){
-        data.forEach(function(value, index, element) {
-        var Counter = index;
-        var title = value.title;
-        var artist = value.artist;
-        var album = value.album;
-        var img = value.img;
-        var songID = value.id;
+  var parseData = function(data) {
+    data.forEach(function(value, index, element) {
+      var Counter = index;
+      var title = value.title;
+      var artist = value.artist;
+      var album = value.album;
+      var img = value.img;
+      var songID = value.id;
 
-        appendDom(title,artist,img,album,Counter);
+      appendDom(title, artist, img, album, Counter);
 
-          $('#song-num'+Counter).on('click',function(){
-            $('#song-num'+Counter).hide('slow/400/fast', function() {
-              location.reload();
-            });
-
-            var songData = {'songID':songID};
-            $.ajax({
-              url: 'https://quedj.herokuapp.com/deleteQueData',
-              type: 'DELETE',
-              data: songData
-            })
-          })
+      $('#song-num' + Counter).on('click', function() {
+        $('#song-num' + Counter).hide('slow/400/fast', function() {
+          location.reload();
         });
 
-      }
+        var songData = {
+          'songID': songID
+        };
+        $.ajax({
+          url: 'https://quedj.herokuapp.com/deleteQueData',
+          type: 'DELETE',
+          data: songData
+        })
+      })
+    });
 
-      var appendDom = function(title,artist,img,album,Counter){
+  }
 
-        $('#listQue').append('<li class="collection-item avatar"> <img src="' + img + '" alt="image" class="circle"> <span class="title">'+title+'</span> <p>' + album+ ' <br>' +'- ' + '<span class = "textColor">'+ artist + '</span>'+'<div class = "row">'+ '<button id = "song-num'+Counter+'"" class = "deleteQue waves-effect waves-light btn btnColor">Delete From Que</button>' +'</div>');
-      }
+  var appendDom = function(title, artist, img, album, Counter) {
+
+    $('#listQue').append('<li class="collection-item avatar"> <img src="' + img + '" alt="image" class="circle"> <span class="title">' + title + '</span> <p>' + album + ' <br>' + '- ' + '<span class = "textColor">' + artist + '</span>' + '<div class = "row">' + '<button id = "song-num' + Counter + '"" class = "deleteQue waves-effect waves-light btn btnColor">Delete From Que</button>' + '</div>');
+  }
 
 });
